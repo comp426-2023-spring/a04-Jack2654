d7a6cf5b531fd28e84026f8dda0f54d9d13b5f3a
Jack2654 <jack.morgenstein@gmail.com>
2023-05-04 23:56:44 -0400

    second commit

--- server.js
+++ server.js
@@ -6,14 +6,7 @@ import { rps, rpsls } from './lib/rpsls.js';
 const args = minimist(process.argv.slice(2));
 const app = express();
 
-if(args.port)
-{
-    const port = args.port
-}
-else
-{
-    const port = 5000
-}
+const port = argv["port"] || 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
