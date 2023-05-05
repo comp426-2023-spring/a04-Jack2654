1a2389653f5c55ea5849109c3f4b150a15f93ade
Jack2654 <jack.morgenstein@gmail.com>
2023-05-04 23:54:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+import minimist from 'minimist';
+import express from 'express';
+import { rps, rpsls } from './lib/rpsls.js';
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+
+if(args.port)
+{
+    const port = args.port
+}
+else
+{
+    const port = 5000
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/app/", (req, res) => {
+    res.status(200).send("200 OK");
+});
+
+app.get("/app/rps/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+});
+
+app.get("/app/rpsls/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+});
+
+app.get("/app/rps/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+});
+
+app.post("/app/rps/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+});
+
+app.get("/app/rpsls/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+});
+
+app.post("/app/rpsls/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+});
+
+app.get("/app/rps/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+});
+
+app.get("/app/rpsls/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND")
+});
+
+app.listen(port, () => {
+    console.log('Server listening on port ${port}');
+});
